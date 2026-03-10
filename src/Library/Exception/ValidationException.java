54afeca5eb273f625c54aa480767d9e712ac5f7c
MAU, King Chung <132557080+godsmartdog@users.noreply.github.com>
2026-03-10 20:27:07 +0800

    Update ValidationException.java

--- src/Library/Exception/ValidationException.java
+++ src/Library/Exception/ValidationException.java
@@ -1,7 +1,10 @@
+// Import the system library used for extension handling
 package Library.Exception;
 
+// We use inheritance to perform specialization
 public class ValidationException extends RuntimeException {
-    public ValidationException(String message) { super(message); }
+    // First instance of function overloading to throw exception message, String argument defined elsewhere
+    public ValidationException(String message) { super(message); } // conversion constructor
 }
 
 //It should be used as a general exception, just add in in catch, wont get wrong
