be14c7d00f9a2eb69c61e4389f39caf093136abb
MAU, King Chung <132557080+godsmartdog@users.noreply.github.com>
2026-03-10 21:55:11 +0800

    Update MemoryBookRepository.java

--- src/Library/Repository/MemoryBookRepository.java
+++ src/Library/Repository/MemoryBookRepository.java
@@ -1,21 +1,24 @@
-package Library.Repository;
+//This file will be used in Library.service.BookService 
 
+//Import the system library used for extension handling
+package Library.Repository;
+//import class of book
 import Library.Model.Book;
 import java.util.*;
 
 public class MemoryBookRepository implements BookRepository {
     private final Map<String, Book> books = new HashMap<>();
-
+//we use id as key to store book in map
     @Override
     public void save(Book book) {
         books.put(book.getId(), book);
     }
-
+// we use id as key to find book in map
     @Override
     public Optional<Book> findById(String id) {
         return Optional.ofNullable(books.get(id));
     }
-
+// list all book stored currently
     @Override
     public List<Book> findAll() {
         return new ArrayList<>(books.values());
