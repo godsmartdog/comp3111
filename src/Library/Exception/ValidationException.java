9c285445b571b68d1e9321a50f332f2a9ab78cd7
MAU, King Chung <132557080+godsmartdog@users.noreply.github.com>
2026-03-10 19:39:32 +0800

    task2 comment validationexception comment
    Removed unnecessary comment about usage.

--- src/Library/Exception/ValidationException.java
+++ src/Library/Exception/ValidationException.java
@@ -4,4 +4,4 @@ public class ValidationException extends RuntimeException {
     public ValidationException(String message) { super(message); }
 }
 
-
+//It should be used as a general exception, just add in in catch, wont get wrong
