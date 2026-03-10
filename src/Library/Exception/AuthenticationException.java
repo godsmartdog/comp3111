fe4d4dd8a5b9b8337300bdc46f53b9b930d787bc
LAM, Yuen Hei <yhlamaw@connect.ust.hk>
2026-03-10 20:19:53 +0800

    Comment - Enhance AuthenticationException with function overloading
    Added function overloading to AuthenticationException for detailed error handling.

--- src/Library/Exception/AuthenticationException.java
+++ src/Library/Exception/AuthenticationException.java
@@ -1,12 +1,17 @@
-package Library.Exception;
+package Library.Exception; // Import the system library used for extension handling
 
+// We use inheritance to perform specialization
 public class AuthenticationException extends RuntimeException {
 
-    public AuthenticationException(String message) {
+    // First instance of function overloading to throw exception message, String argument defined elsewhere
+    public AuthenticationException(String message) { 
         super(message);
     }
+
+    // Second instance of function overloading, now with Throwable argument for detailed error message with tracable cause
     public AuthenticationException(String message, Throwable cause) {
         super(message, cause);
     }
 }
+
 //used in UI to handle error
