e211a0b6188391aabdfa6b471bc4bf9e4710742b
LAM, Yuen Hei <yhlamaw@connect.ust.hk>
2026-03-10 21:37:32 +0800

    Comment - Add comment to Role enum for clarity
    Added comment to clarify the purpose of the Role enum.

--- src/Library/Model/Role.java
+++ src/Library/Model/Role.java
@@ -1,5 +1,6 @@
-package Library.Model;
+package Library.Model; // imported in BookRepository for interface
 
+// the enum class is more representative than indexes
 public enum Role {
     STUDENT,
     STAFF,
