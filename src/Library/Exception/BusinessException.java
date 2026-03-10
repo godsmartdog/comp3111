75ff122c74d19c18605d62e3a149a2463c952f94
MAU, King Chung <132557080+godsmartdog@users.noreply.github.com>
2026-03-10 20:21:52 +0800

    Enhance BusinessException with descriptive comments
    Added comments to clarify the purpose of the BusinessException class and its constructors.

--- src/Library/Exception/BusinessException.java
+++ src/Library/Exception/BusinessException.java
@@ -1,11 +1,12 @@
-package Library.Exception;
+package Library.Exception;// Import the system library used for extension handling
 
+// We use inheritance to perform specialization
 public class BusinessException extends RuntimeException {
-
+// First instance of function overloading to throw exception message, String argument defined elsewhere
     public BusinessException(String message) {
         super(message);
     }
-
+//// Second instance of function overloading, now with Throwable argument for detailed error message with tracable cause
     public BusinessException(String message, Throwable cause) {
         super(message, cause);
     }
