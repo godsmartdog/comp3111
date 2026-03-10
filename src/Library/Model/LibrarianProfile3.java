47e79ddef066cd00b1c0fdbae284116a37a7f963
LAM, Yuen Hei <yhlamaw@connect.ust.hk>
2026-03-10 21:35:26 +0800

    Comment - Refactor LibrarianProfile3 with comments and package update
    Updated package declaration and added comments for clarity.

--- src/Library/Model/LibrarianProfile3.java
+++ src/Library/Model/LibrarianProfile3.java
@@ -1,14 +1,19 @@
-package Library.Model;
+package Library.Model; // imported in BookRepository for interface
 
+// Stores the information of librarians who consider and permits books
 public class LibrarianProfile3 {
+
+    // Member variables
     private final String username;
     private final String employeeId;
 
+    // Constructor
     public LibrarianProfile3 (String username, String employeeId) {
         this.username = username;
         this.employeeId = employeeId;
     }
 
+    // Accessor
     public String getUsername() { return username; }
     public String getEmployeeId() { return employeeId; }
 }
