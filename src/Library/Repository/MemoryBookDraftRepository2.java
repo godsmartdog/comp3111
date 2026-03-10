6e7a69cf78620228444d50a31242b0d34c7e0aa9
MAU, King Chung <132557080+godsmartdog@users.noreply.github.com>
2026-03-10 21:46:33 +0800

    Update MemoryBookDraftRepository2.java

--- src/Library/Repository/MemoryBookDraftRepository2.java
+++ src/Library/Repository/MemoryBookDraftRepository2.java
@@ -1,24 +1,31 @@
 // [Task 2]
-package Library.Repository;
+//This file will be used in Library.service.AuthorDraftService
 
+// Import the system library used for extension handling
+package Library.Repository;
+//import class
 import Library.Model.BookDraft2;
+//import map for storing
 import java.util.HashMap;
 import java.util.Map;
+//safe return
 import java.util.Optional;
 
 public class MemoryBookDraftRepository2 implements BookDraftRepository2 {
+    //create map to ctore
     private final Map<String, BookDraft2> map = new HashMap<>();
 
+    //we take AuthorUsername as key for draft 
     @Override
     public void save(BookDraft2 draft) {
         map.put(draft.getAuthorUsername(), draft);
     }
-
+    //we use authorUsername as key to find draft
     @Override
     public Optional<BookDraft2> findByAuthorUsername(String authorUsername) {
         return Optional.ofNullable(map.get(authorUsername));
     }
-
+    //we use authorUsername as key to delete draft
     @Override
     public void deleteByAuthorUsername(String authorUsername) {
         map.remove(authorUsername);
