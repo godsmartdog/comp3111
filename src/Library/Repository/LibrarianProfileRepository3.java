58efef717df00ddd6ae844c26e2334fc69006bc4
MAU, King Chung <132557080+godsmartdog@users.noreply.github.com>
2026-03-10 21:29:46 +0800

    Update LibrarianProfileRepository3.java

--- src/Library/Repository/LibrarianProfileRepository3.java
+++ src/Library/Repository/LibrarianProfileRepository3.java
@@ -1,7 +1,11 @@
+//This file will be used in Library.service./LibrarianService3.java
+
+//function( save(), findByUsername()) defined in Memoryxxxx -> just go see this
+// Import the system library used for extension handling
 package Library.Repository;
-
+//import class from LibrarianProfile3
 import Library.Model.LibrarianProfile3;
-
+//safe return
 import java.util.Optional;
 
 public interface LibrarianProfileRepository3 {
