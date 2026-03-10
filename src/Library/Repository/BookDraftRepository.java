899485f6ce841c8aeaf25e481dfd54f62ee8f2c6
MAU, King Chung <132557080+godsmartdog@users.noreply.github.com>
2026-03-10 20:47:01 +0800

    Update BookDraftRepository.java

--- src/Library/Repository/BookDraftRepository.java
+++ src/Library/Repository/BookDraftRepository.java
@@ -1,9 +1,14 @@
 // [Task 2]
+//This file will be used in Library.service.AuthorDraftService
+//function defined in Memoryxxxx -> just go see this
+// Import the system library used for extension handling
 package Library.Repository;
-
+//import the class
 import Library.Model.BookDraft2;
+//safe return
 import java.util.Optional;
 
+//declare function
 public interface BookDraftRepository2 {
     void save(BookDraft2 draft);
     Optional<BookDraft2> findByAuthorUsername(String authorUsername);
