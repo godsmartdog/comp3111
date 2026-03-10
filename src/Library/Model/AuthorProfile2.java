7849ab44ad8b056193f45f1a3f47dd78fe1c7037
LAM, Yuen Hei <yhlamaw@connect.ust.hk>
2026-03-10 20:27:57 +0800

    Update AuthorProfile2.java

--- src/Library/Model/AuthorProfile2.java
+++ src/Library/Model/AuthorProfile2.java
@@ -1,16 +1,18 @@
-package Library.Model;
-//imported to AuthorProfileRepository2.java for interface
-//Profile for Author
-//para: username,bio
-public class AuthorProfile2 {
+package Library.Model; //imported to AuthorProfileRepository2.java for interface
+
+public class AuthorProfile2 { // Profile for Author with parameters: {username, bio}
+
+    // Member variables - final keyword for immutability
     private final String username;
     private final String bio;
 
+    // Initialize the member variables or update data
     public AuthorProfile2(String username, String bio) {
         this.username = username;
         this.bio = bio;
     }
 
+    // Accessor
     public String getUsername() { return username; }
     public String getBio() { return bio; }
 }
