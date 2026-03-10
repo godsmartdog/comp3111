9780d53a2a56fb02a61486e4950d333f0c1b8c7d
MAU, King Chung <132557080+godsmartdog@users.noreply.github.com>
2026-03-10 21:22:37 +0800

    Update BookSubmissionRepository2.java

--- src/Library/Repository/BookSubmissionRepository2.java
+++ src/Library/Repository/BookSubmissionRepository2.java
@@ -1,9 +1,14 @@
+//This file will be used in Library.service.LibrarianService3
+//all function( save(), findById(), findall(), findBystatus(), findByAuthorUsername()) defined in Memoryxxxx -> just go see this
+// Import the system library used for extension handling
 package Library.Repository;
-
+//for class of BookSubmission2
 import Library.Model.BookSubmission2;
+//data type Submission state
 import Library.Model.SubmissionState;
-
+//list to store collections
 import java.util.List;
+//safe return
 import java.util.Optional;
 
 public interface BookSubmissionRepository2 {
