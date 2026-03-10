08cb87871c75e102b108ef8afb66b5a1ba9c7d10
MAU, King Chung <132557080+godsmartdog@users.noreply.github.com>
2026-03-10 20:29:23 +0800

    Update AuthorProfileRepository2.java

--- src/Library/Repository/AuthorProfileRepository2.java
+++ src/Library/Repository/AuthorProfileRepository2.java
@@ -1,10 +1,13 @@
+// Import the system library used for extension handling
 package Library.Repository;
-
+//import the class of AuthorProfile2
 import Library.Model.AuthorProfile2;
 
 import java.util.Optional;
 
 public interface AuthorProfileRepository2 {
+    //save the profile
     void save(AuthorProfile2 profile);
+    //username is para in profile, AuthorProfile2 object should be returned
     Optional<AuthorProfile2> findByUsername(String username);
 }
