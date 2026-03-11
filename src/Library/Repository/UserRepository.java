c967b9a16b87663a9dca4ca910731b0e9a55b14b
MAU, King Chung <132557080+godsmartdog@users.noreply.github.com>
2026-03-11 14:57:48 +0800

    Update UserRepository.java

--- src/Library/Repository/UserRepository.java
+++ src/Library/Repository/UserRepository.java
@@ -1,9 +1,9 @@
 package Library.Repository;
-
+//class user
 import Library.Model.User;
 import java.util.List;
 import java.util.Optional;
-
+//just go check Memoryxxx this just interface
 public interface UserRepository {
     Optional<User> findByUsername(String username);
     void save(User user);
