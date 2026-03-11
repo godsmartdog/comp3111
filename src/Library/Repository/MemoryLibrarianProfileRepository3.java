2bc5641968bc2acd94abccee9e2e2145023d4ec6
MAU, King Chung <132557080+godsmartdog@users.noreply.github.com>
2026-03-11 14:55:56 +0800

    Update MemoryLibrarianProfileRepository3.java

--- src/Library/Repository/MemoryLibrarianProfileRepository3.java
+++ src/Library/Repository/MemoryLibrarianProfileRepository3.java
@@ -1,18 +1,20 @@
 package Library.Repository;
-
+//class used
 import Library.Model.LibrarianProfile3;
+//way to store
 import java.util.HashMap;
 import java.util.Map;
+//safe return
 import java.util.Optional;
 
 public class MemoryLibrarianProfileRepository3 implements LibrarianProfileRepository3 {
     private final Map<String, LibrarianProfile3> map = new HashMap<>();
-
+    //we use username as key
     @Override
     public void save(LibrarianProfile3 profile) {
         map.put(profile.getUsername(), profile);
     }
-
+    //we use key to search
     @Override
     public Optional<LibrarianProfile3> findByUsername(String username) {
         return Optional.ofNullable(map.get(username));
