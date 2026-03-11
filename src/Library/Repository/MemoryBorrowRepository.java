65356c112438ae7219fc43d50d409f6039dd1e4d
MAU, King Chung <132557080+godsmartdog@users.noreply.github.com>
2026-03-11 14:55:02 +0800

    Update MemoryBorrowRepository.java

--- src/Library/Repository/MemoryBorrowRepository.java
+++ src/Library/Repository/MemoryBorrowRepository.java
@@ -1,33 +1,36 @@
 package Library.Repository;
-
+//class of borrowRecord
 import Library.Model.BorrowRecord;
+//list to store collections
 import java.util.ArrayList;
 import java.util.List;
+//safe return
 import java.util.Optional;
+//we return this
 import java.util.stream.Collectors;
 
 public class MemoryBorrowRepository implements BorrowRepository {
     private final List<BorrowRecord> records = new ArrayList<>();
-
+    //add record in records
     @Override
     public void save(BorrowRecord record) {
         records.add(record);
     }
-
+    //just search the first match one
     @Override
     public Optional<BorrowRecord> findById(String id) {
         return records.stream()
                 .filter(r -> r.getId().equals(id))
                 .findFirst();
     }
-
+    //all match name will be shown
     @Override
     public List<BorrowRecord> findByUsername(String username) {
         return records.stream()
                 .filter(r -> r.getUsername().equals(username))
                 .collect(Collectors.toList());
     }
-
+    //everything will be shown
     @Override
     public List<BorrowRecord> findAll() {
         return new ArrayList<>(records);
